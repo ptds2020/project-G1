c0eef92bb2bcfe0e74d5e6a397cba54b463acba6
GaetanLovey <gaetan.lovey@unil.ch>
2020-12-11 18:20:59 +0100

    Modify files to resolve run problem

--- R/run.R
+++ R/run.R
@@ -1,8 +1,9 @@
 #' @title Run the McDonald shiny app
 #' @export
+#' @import shiny dplyr readxl ggplot2 tidyr kableExtra here shinydashboard reshape2 plotly ECharts2Shiny shinyWidgets
 
 run <- function() {
-  appDir <- system.file("McDonald", package = "McDonald")
+  appDir <- system.file("shiny-examples", "McDonald", package = "McDonald")
   if (appDir == "") {
     stop(
       "Could not find directory. Try re-installing MacDonald.",
